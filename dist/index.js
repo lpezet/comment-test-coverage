938b5f0c3334f1015907b8b63141af16fe30f273
Luke Pezet <lpezet@gmail.com>
2024-09-13 20:26:06 -0600

    Fixed formatting.

--- dist/index.js
+++ dist/index.js
@@ -31122,13 +31122,13 @@ async function run() {
       commentFrom: inputs.id,
     };
     const coverage = `<!--json:${JSON.stringify(meta)}-->
-    |${inputs.title}| %                           | values                                                              |
-    |---------------|:---------------------------:|:-------------------------------------------------------------------:|
-    |Statements     |${json.total.statements.pct}%|( ${json.total.statements.covered} / ${json.total.statements.total} )|
-    |Branches       |${json.total.branches.pct}%  |( ${json.total.branches.covered} / ${json.total.branches.total} )    |
-    |Functions      |${json.total.functions.pct}% |( ${json.total.functions.covered} / ${json.total.functions.total} )  |
-    |Lines          |${json.total.lines.pct}%     |( ${json.total.lines.covered} / ${json.total.lines.total} )          |
-    `;
+|${inputs.title}| %                           | values                                                              |
+|---------------|:---------------------------:|:-------------------------------------------------------------------:|
+|Statements     |${json.total.statements.pct}%|( ${json.total.statements.covered} / ${json.total.statements.total} )|
+|Branches       |${json.total.branches.pct}%  |( ${json.total.branches.covered} / ${json.total.branches.total} )    |
+|Functions      |${json.total.functions.pct}% |( ${json.total.functions.covered} / ${json.total.functions.total} )  |
+|Lines          |${json.total.lines.pct}%     |( ${json.total.lines.covered} / ${json.total.lines.total} )          |
+`;
 
     await createOrUpdateComment({
       id: inputs.id,
